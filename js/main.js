5303b47caf8c21a2cf84fc89c9a211a158cb5125
HildebrandJakob <60198323+HildebrandJakob@users.noreply.github.com>
2020-03-11 14:59:47 -0500

    Activity 8 main.js

--- js/main.js
+++ js/main.js
@@ -29,6 +29,17 @@ window.onload = function(){
     var maxPop = d3.max(cityPop, function(d){
         return d.population;
     });
+
+    //color scale generation
+    var color = d3.scaleLinear()
+          .range([
+            "#FDBE85",
+            "#D94701"
+          ])
+          .domain([
+            minPop,
+            maxPop
+          ]);
   //SVG dimension variables
   var w = 900, h = 500;
 
@@ -37,7 +48,7 @@ window.onload = function(){
     .attr('width', w) //assigns w to width
     .attr('height', h) //assigns h to height
     .attr("class", "container") //assigns class name
-    .style("background-color", "rgba(5,5,30,0.4)");
+    .style("background-color", "rgba(0,0,0,0.2)");
 
   var innerRect = container.append("rect") //put rect in svg
   .datum(400) //a single value is a DATUM
@@ -54,15 +65,12 @@ window.onload = function(){
 
   //var dataArray = [10, 20, 30, 40, 50];
   var x = d3.scaleLinear() //create the scale
-        .range([90, 810]) //output min and max
+        .range([90, 760]) //output min and max
         .domain([0, 3]); //input min and max
         //scale for circles center y coordinate
-  var y = d3.scaleLinear()
-        .range([440, 95])
-        .domain([
-          minPop,
-          maxPop
-          ]);
+  var y = d3.scaleLinear() //y-scale (linear)
+        .range([450, 50])
+        .domain([0, 700000]);
   var circles = container.selectAll(".circles") //but wait--there are no circles yet!
       .data(cityPop) //here we feed in an array
       .enter() //one of the great mysteries of the universe
@@ -82,6 +90,58 @@ window.onload = function(){
       .attr("cy", function(d, i){
         return y(d.population);
       })
+      .style("fill", function(d, i){ //color fill from color scale
+        return color(d.population)
+      })
+      .style("stroke", "#000"); //black stroke on circle
+
+  var yAxis = d3.axisLeft(y);
+  var axis = container.append("g") //create axis g element
+    .attr("class", "axis")
+    .attr("transform", "translate(50,0)") //pushes axis to right to fit
+    .call(yAxis);
+
+  var title = container.append("text")
+    .attr("class", "title")
+    .attr("text-anchor", "middle") //sets text anchorpoint to mid, not left
+    .attr("x", 450)
+    .attr("y", 30)
+    .text("Wisconsin City Populations"); //sets title, see style.css for setup
+  //create format generator
+  var format = d3.format(",");
+  //create labels
+  var labels = container.selectAll(".labels")
+    .data(cityPop)
+    .enter()
+    .append("text")
+    .attr("class", "labels")
+    .attr("text-anchor", "left")
+    .attr("y", function(d){
+      //vertical position of label on each circle
+      return y(d.population)
+    });
+  //first line of labels
+  var nameLine = labels.append("tspan")
+    .attr("class", "nameLine")
+    .attr("x", function(d,i){
+      //horizontal position of labels
+      return x(i)+Math.sqrt(d.population*0.01/Math.PI)+5;
+    })
+    .text(function(d){
+      return d.city;
+    });
+  //second line of labels
+  var popLine = labels.append("tspan")
+    .attr("class", "popLine")
+    .attr("x", function(d,i){
+      //horizontal positon of labels
+      return x(i)+Math.sqrt(d.population*0.01/Math.PI)+5;
+    })
+    .attr("dy", "15") //vertical offset
+    .text(function(d){
+      return "Pop. " + format(d.population);
+    });
+
   console.log(innerRect)
   console.log(container)
 };
