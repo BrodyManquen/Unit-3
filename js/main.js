2ce66ca9b25dc245485f222aacc2adfa450810d6
Brody W. Manquen <60198313+BrodyManquen@users.noreply.github.com>
2020-03-31 21:29:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,15 @@
 window.onload = setMap();
 
 function setMap(){
-  var promises = [d3.csv("data")]
+  var promises = [d3.csv("data/chicagoCensus.csv"),
+                  d3.json("data/chicagoCensusTopoJSON.json")
+                ];
+  Promise.all(promises).then(callback);
+
+  function callback(data){
+    csvData = data[0];
+    chicagoZIP = data[1];
+    console.log(csvData);
+    console.log(chicagoZIP);
+};
 }
